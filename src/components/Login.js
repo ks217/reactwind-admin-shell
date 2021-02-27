df9474baaec0fb948b4cd2cb89c878870756c758
Kartik Suthar <kartik@synage.com>
2021-02-27 18:15:57 +0530

    Added app name near logo

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,18 +4,21 @@ export default function Login(props) {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        <div>
+        <div className="flex flex-row justify-center space-x-4 items-center">
           <img
-            className="mx-auto h-12 w-auto"
+            className="h-12"
             src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
             alt="Workflow"
           />
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
-          </h2>
+          <h1 className="text-center text-3xl font-extrabold text-indigo-600">
+            My App
+          </h1>
         </div>
+        <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">
+          Sign in to your account
+        </h2>
         <form
-          className="mt-8 space-y-6 text-left bg-white px-8 py-8 rounded-md shadow"
+          className="mt-8 space-y-6 text-left bg-white p-8 rounded-md shadow"
           action="#"
           method="POST"
         >
