4b9369cd4df2b50d1b5874d11a5136509c0bca40
Kartik Suthar <kartik@synage.com>
2021-02-27 21:16:26 +0530

    Added dashboard, login routes. A mini 404 page when no route matches

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import Login from './pages/Login';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+  Link,
+} from 'react-router-dom';
 function App() {
   return (
-    <div className="text-center">
-      <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/dashboard">
+          <div>Dashboard</div>
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Redirect exact from="/" to="/login" />
+        <Route path="*">
+          <div className="min-h-screen flex flex-col justify-center">
+            <div>
+              <h2 className="text-center text-xl text-gray-900">
+                &#x1F6AA; Page Not Found!
+              </h2>
+              <Link to="/login">
+                <h2 className="text-center text-xl text-gray-900 mt-4">
+                  &#x1F3E1; Click here to go Home!
+                </h2>
+              </Link>
+            </div>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
