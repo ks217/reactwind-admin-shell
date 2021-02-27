77bdaeb1c698787bc14f75e07aa8a035507488a7
Kartik Suthar <kartik@synage.com>
2021-02-27 16:51:28 +0530

    Sample Tailwind Login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import Login from './components/Login';
+
 function App() {
-	return (
-		<div className="text-center">
-			<header className="bg-yellow-50 dark:bg-gray-800 min-h-screen flex flex-col justify-center items-center">
-				<p className=" text-gray-500">
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-			</header>
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
