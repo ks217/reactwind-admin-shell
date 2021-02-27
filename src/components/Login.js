2fb55adf6a94a6a176b4c8fe803c0eb5cc84b8b2
Kartik Suthar <kartik@synage.com>
2021-02-27 18:57:35 +0530

    Improvements: removed bold "sign in.." and moved forgot link

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ export default function Login(props) {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        <div className="flex flex-row justify-center space-x-4 items-center">
+        <div className="flex flex-row space-x-4 justify-center items-center">
           <img
             className="h-12"
             src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
@@ -14,98 +14,90 @@ export default function Login(props) {
             My App
           </h1>
         </div>
-        <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">
-          Sign in to your account
-        </h2>
-        <form
-          className="mt-8 space-y-6 text-left bg-white p-8 rounded-md shadow"
-          action="#"
-          method="POST"
-        >
-          <label className="block">
-            <span className="text-gray-700">Email address</span>
-            <input
-              type="email"
-              id="email-address"
-              name="email"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              placeholder="john@example.com"
-              required
-              autoComplete="email"
-            />
-          </label>
-          <label className="block">
-            <span className="text-gray-700">Password</span>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              autoComplete="current-password"
-              required
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              placeholder="Password"
-            />
-          </label>
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
+        <div>
+          <h2 className="mt-6 text-center text-2xl  text-gray-900">
+            Sign in to your account
+          </h2>
+          <form
+            className="mt-8 space-y-6 text-left bg-white p-8 rounded-md shadow"
+            action="#"
+            method="POST"
+          >
+            <label className="block">
+              <span className="text-gray-700">Email address</span>
               <input
-                id="remember_me"
-                name="remember_me"
-                type="checkbox"
-                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                type="email"
+                id="email-address"
+                name="email"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                placeholder="john@example.com"
+                required
+                autoComplete="email"
               />
-              <label
-                htmlFor="remember_me"
-                className="ml-2 block text-sm text-gray-900"
-              >
-                Remember me
-              </label>
-            </div>
-            {/* <div class="text-sm">
-              <a
-                href="#"
-                class="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                Forgot your password?
-              </a>
-            </div> */}
-            <input type="hidden" name="remember" value="true" />
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <svg
-                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
+            </label>
+            <label className="block">
+              <span className="text-gray-700">Password</span>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                placeholder="Password"
+              />
+            </label>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember_me"
+                  name="remember_me"
+                  type="checkbox"
+                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember_me"
+                  className="ml-2 block text-sm text-gray-900"
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </span>
-              Sign in
-            </button>
-          </div>
-          <div>
-            <div class="text-sm">
-              <a
-                href="#forgot-password"
-                class="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                Forgot your password?
-              </a>
+                  Remember me
+                </label>
+              </div>
+              <div class="text-sm">
+                <a
+                  href="#forgot-password"
+                  class="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <input type="hidden" name="remember" value="true" />
             </div>
-          </div>
-        </form>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <svg
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
