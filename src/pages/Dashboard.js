1b7e47f4940474fbf41d8f171e30f0f5260a9083
Kartik <kartik@synage.com>
2021-05-15 20:14:41 +0530

    Changes to light and dark mode colors

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,10 +2,10 @@ import React from 'react';
 const Dashboard = () => {
   return (
     <>
-      <h2 className="text-left text-xl">List of stuff</h2>
+      <h2 className="text-left text-xl">Some stuff</h2>
       <div className="max-w-7xl mx-auto">
         <div className="py-6">
-          <div className="border-4 border-dashed border-indigo-200 bg-indigo-100 rounded-lg h-96"></div>
+          <div className="border-4 border-dashed border-gray-200 bg-gray-100 dark:border-gray-700 dark:bg-gray-800 rounded-lg h-96"></div>
         </div>
       </div>
     </>
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -2,10 +2,10 @@ import React from 'react';
 const Project = () => {
   return (
     <>
-      <h2 className="text-left text-xl">List of projects</h2>
+      <h2 className="text-left text-xl">Some events</h2>
       <div className="max-w-7xl mx-auto">
         <div className="py-6">
-          <div className="border-4 border-dashed border-gray-200 bg-gray-100 rounded-lg h-96"></div>
+          <div className="border-4 border-dashed border-gray-200 bg-gray-100 dark:border-gray-700 dark:bg-gray-800 rounded-lg h-96"></div>
         </div>
       </div>
     </>
